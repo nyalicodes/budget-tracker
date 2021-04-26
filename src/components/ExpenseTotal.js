1fab86dee21e6dbfceea31e5c2a42a6b7efd2dc6
Nyaladzi Trust Tjibha <tjibhanyaladzi@gmail.com>
2021-04-26 12:42:19 +0200

    App components created

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ExpenseTotal = () => {
+    return (
+        <div className="alert alert-primary">
+            <span>
+                Spent so far: R800
+            </span>
+        </div>
+    )
+}
+
+export default ExpenseTotal
